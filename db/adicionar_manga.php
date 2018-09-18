e24a0b0b9b63f0ea6f7e0450dc21105ade3180ac
lmgcastro <42627912+lmgcastro@users.noreply.github.com>
2018-09-18 13:33:17 -0300

    Tab removed

--- db/adicionar_manga.php
+++ db/adicionar_manga.php
@@ -4,18 +4,18 @@
     $titulo = $_POST['titulo'];
     $volume = $_POST['volume'];
 	
-	// SELECT DADOS NAO INFORMADOS
-	$sql = "SELECT DISTINCT Artistas, Editora, Original, Total FROM mangas WHERE Titulo = '$titulo';";
-	$result = mysqli_query($conn, $sql);
-	$resultCheck = mysqli_num_rows($result);
+    // SELECT DADOS NAO INFORMADOS
+    $sql = "SELECT DISTINCT Artistas, Editora, Original, Total FROM mangas WHERE Titulo = '$titulo';";
+    $result = mysqli_query($conn, $sql);
+    $resultCheck = mysqli_num_rows($result);
 
-	if ($resultCheck > 0) {
-		while ($row = mysqli_fetch_assoc($result)) {
-			$artistas = $row['Artistas'];
-			$editora = $row['Editora'];
-			$original = $row['Original'];
-			$total = $row['Total'];
-		}
+    if ($resultCheck > 0) {
+        while ($row = mysqli_fetch_assoc($result)) {
+            $artistas = $row['Artistas'];
+            $editora = $row['Editora'];
+            $original = $row['Original'];
+            $total = $row['Total'];
+        }
     }
 
     $sql = "INSERT INTO mangas VALUES ('$titulo', '$volume', '$artistas', '$editora', '$original', 'N', $total);";
